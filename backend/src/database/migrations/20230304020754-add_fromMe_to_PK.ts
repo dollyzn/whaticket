75afe716e01e9ed433859d70668058589d4eba95
Natã <66445721+dollyzn@users.noreply.github.com>
2023-03-16 13:21:19 -0300

    rectify down query command interface

--- backend/src/database/migrations/20230304020754-add_fromMe_to_PK.ts
+++ backend/src/database/migrations/20230304020754-add_fromMe_to_PK.ts
@@ -2,24 +2,10 @@ import { QueryInterface, DataTypes } from "sequelize";
 
 module.exports = {
   up: (queryInterface: QueryInterface) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
    return queryInterface.sequelize.query("ALTER TABLE Messages DROP PRIMARY KEY, ADD CONSTRAINT Messages_PK PRIMARY KEY (id, fromMe)");
   },
 
   down: (queryInterface: QueryInterface) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-      return queryInterface.sequelize.query('ALTER TABLE Messages DROP CONSTRAINT Messages_PK, ADD PRIMARY KEY (id)');
+      return queryInterface.sequelize.query("ALTER TABLE Messages DROP PRIMARY KEY, ADD CONSTRAINT Messages_PK PRIMARY KEY (id)");
   }
 };
